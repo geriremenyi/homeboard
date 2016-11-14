4c900bea056d13d37b1a12c0136698b88c71e8c9
Gergely Reményi <geri@remenyicsalad.hu>
2016-11-14 01:02:44 +0100

    Correcting test environment

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+// Set timezone
+date_default_timezone_set('Europe/Budapest');
+
+// Prevent session cookies
+ini_set('session.use_cookies', 0);
+
+// Prevent to report warnings
+error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
+
+// Include settings
+require_once('../resty-settings.php');
+
+// Enable Composer autoloader
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Register test classes
+$autoloader->addPsr4('Resty\\', __DIR__ . '/..', true);
+$autoloader->addPsr4('Application\\', __DIR__ . '/..', true);
